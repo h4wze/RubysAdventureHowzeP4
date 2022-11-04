ddf5f13263b75a2a51686f450f3d86093f11c852
h4wze <116380750+h4wze@users.noreply.github.com>
2022-11-04 11:42:51 -0700

    added scenes and tiles.
    added scenes and tiles and added movement.

---
+++ RubysAdventureHowze/Assets/Scripts/RubyController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //create 2 variables to use the unity build in axes
+        float horizontal = Input.GetAxis("Horizontal")
+        float vertical = Input.GetAxis("Vertical");
+
+        //Create my movement vector
+        Vector2 position = transform.position;
+
+        //Create the movement of my character
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //Set the new position
+        transform.position = position;
+    }
+}
